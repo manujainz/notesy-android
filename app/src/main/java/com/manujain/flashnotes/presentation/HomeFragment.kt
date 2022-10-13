868916649e4981ac082354968605091f2a8e6795
Manu Jain <manujainpixels@gmail.com>
2022-10-13 22:42:51 +0530

    removed unused imports

--- app/src/main/java/com/manujain/flashnotes/presentation/HomeFragment.kt
+++ app/src/main/java/com/manujain/flashnotes/presentation/HomeFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.manujain.flashnotes.R
 import com.manujain.flashnotes.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
