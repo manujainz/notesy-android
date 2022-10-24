c1b05884cdf526426c1a5391ea77ca27352844c8
Manu Jain <manujainpixels@gmail.com>
2022-10-25 00:09:27 +0530

    NoteContentDiff Util Logic correction

--- app/src/main/java/com/manujain/flashnotes/domain/utils/Utils.kt
+++ app/src/main/java/com/manujain/flashnotes/domain/utils/Utils.kt
@@ -7,5 +7,7 @@ object Utils {
     fun didNoteContentChange(
         note: Note,
         noteState: NoteState
-    ) = (note.title == noteState.title && note.content == noteState.content)
+    ) = note.title != noteState.title ||
+        note.content != noteState.content ||
+        note.color != noteState.color
 }
